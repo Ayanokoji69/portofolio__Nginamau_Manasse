b4d36cc9bd903107f07da57756873e627df63c72
Ayanokoji69 <113607360+Ayanokoji69@users.noreply.github.com>
2022-10-07 23:12:51 +0200

    portfolio 07/10
    reglage probleme de scroll

--- Personal/assets/js/main.js
+++ Personal/assets/js/main.js
@@ -250,18 +250,7 @@
   new PureCounter();
 
 })()
-/**
-   * Hero type effect
-   */
- const typed = select('.typed')
- if (typed) {
-   let typed_strings = typed.getAttribute('data-typed-items')
-   typed_strings = typed_strings.split(',')
-   new Typed('.typed', {
-     strings: typed_strings,
-     loop: true,
-     typeSpeed: 50,
-     backSpeed: 35,
-     backDelay: 2000
-   });
- }
+
+
+
+ 
